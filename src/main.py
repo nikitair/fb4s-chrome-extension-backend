77b706e7d6165a2932671d7935275d86da79645e
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-06 16:21:32 +0300

    Initialize logging

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
 import uvicorn
 from fastapi import FastAPI
+from logs.logging_config import logger
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def startup_event():
+    logger.warning(" == SERVER STARTED ==")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.warning("== SERVER STOPPED ==")
+
+
 @app.get('/')
-def index_view():
+async def index_view():
+    logger.info(f"{index_view.__name__} -- INDEX VIEW TRIGGERED")
     return {
         "service": "FB4S Automations",
         "success": True
     }
 
 if __name__ == "__main__":
-    # test server launcher
-    uvicorn.run(app=app, host="127.0.0.1", port=8000)
+    # dev server run
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
