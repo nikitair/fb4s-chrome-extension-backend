404b979a2507598bb75c10691d74d09035f73078
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-06 16:02:33 +0300

    Initialize server

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index_view():
+    return {
+        "service": "FB4S Automations",
+        "success": True
+    }
+
+if __name__ == "__main__":
+    # test server launcher
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
