4b82a72601e3f90fc2c9fb84ca456e70d64423c3
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-13 19:01:02 +0300

    New FUB methods

--- src/fub/fub.py
+++ src/fub/fub.py
@@ -36,9 +36,9 @@ class FUB:
         return data
     
 
-    def get_people(self, people_id: int):
-        logger.info(f"{self.__class__.__name__} ( {self.get_people.__name__} ) -- GETTING PEOPLE WITH ID: {people_id}")
-        url = f"{self.base_url}people/{people_id}"
+    def get_people(self, person_id: int):
+        logger.info(f"{self.__class__.__name__} ( {self.get_people.__name__} ) -- GETTING PERSON WITH ID: {person_id}")
+        url = f"{self.base_url}people/{person_id}"
         headers = {
             "accept": "application/json",
             "authorization": f"Basic {self.api_key}"
@@ -57,4 +57,107 @@ class FUB:
             logger.exception(f"{self.__class__.__name__} ( {self.get_people.__name__} ) -- !!! FUB API ERROR - {ex}")
 
         return data
+    
 
+    def get_note(self, note_id: int):
+        logger.info(f"{self.__class__.__name__} ( {self.get_note.__name__} ) -- GETTING NOTE WITH ID: {note_id}")
+        url = f"{self.base_url}notes/{note_id}"
+        headers = {
+            "accept": "application/json",
+            "authorization": f"Basic {self.api_key}"
+        }
+
+        data = None
+
+        try:
+            response = requests.get(url, headers=headers)
+            logger.info(f"{self.__class__.__name__} ( {self.get_note.__name__} ) -- FUB API STATUS CODE - {response.status_code}")
+
+            data = response.json()
+            logger.debug(f"{self.__class__.__name__} ( {self.get_note.__name__} ) -- FUB API DATA - {data}")
+
+        except Exception as ex:
+            logger.exception(f"{self.__class__.__name__} ( {self.get_note.__name__} ) -- !!! FUB API ERROR - {ex}")
+
+        return data
+    
+
+    def create_note(self, person_id: int, note_subject: str, note_body: str):
+        logger.info(f"{self.__class__.__name__} ( {self.create_note.__name__} ) -- CREATING NOTE - PERSON_ID: {person_id}; SUBJECT: {note_subject}; BODY: {note_body}")
+        url = f"{self.base_url}notes"
+        headers = {
+            "accept": "application/json",
+            "authorization": f"Basic {self.api_key}"
+        }
+        payload = {
+            "personId": {person_id},
+            "subject": {note_subject},
+            "body": {note_body},
+            "isHtml": True
+        }
+
+        data = None
+
+        try:
+            response = requests.post(url, headers=headers, json=payload)
+            logger.info(f"{self.__class__.__name__} ( {self.create_note.__name__} ) -- FUB API STATUS CODE - {response.status_code}")
+
+            data = response.json()
+            logger.debug(f"{self.__class__.__name__} ( {self.create_note.__name__} ) -- FUB API DATA - {data}")
+
+        except Exception as ex:
+            logger.exception(f"{self.__class__.__name__} ( {self.create_note.__name__} ) -- !!! FUB API ERROR - {ex}")
+
+        return data
+    
+
+    def update_note(self, note_id: int, person_id: int, note_subject: str, note_body: str):
+        logger.info(f"{self.__class__.__name__} ( {self.update_note.__name__} ) -- UPDATING NOTE WITH ID: {note_id} - PERSON_ID: {person_id}; SUBJECT: {note_subject}; BODY: {note_body}")
+        url = f"{self.base_url}notes/{note_id}"
+        headers = {
+            "accept": "application/json",
+            "authorization": f"Basic {self.api_key}"
+        }
+        payload = {
+            "personId": {person_id},
+            "subject": {note_subject},
+            "body": {note_body},
+            "isHtml": True
+        }
+
+        data = None
+
+        try:
+            response = requests.put(url, headers=headers, json=payload)
+            logger.info(f"{self.__class__.__name__} ( {self.update_note.__name__} ) -- FUB API STATUS CODE - {response.status_code}")
+
+            data = response.json()
+            logger.debug(f"{self.__class__.__name__} ( {self.update_note.__name__} ) -- FUB API DATA - {data}")
+
+        except Exception as ex:
+            logger.exception(f"{self.__class__.__name__} ( {self.update_note.__name__} ) -- !!! FUB API ERROR - {ex}")
+
+        return data
+
+
+    def delete_note(self, note_id: int):
+        logger.info(f"{self.__class__.__name__} ( {self.delete_note.__name__} ) -- DELETING NOTE WITH ID: {note_id}")
+        url = f"{self.base_url}notes/{note_id}"
+        headers = {
+            "accept": "application/json",
+            "authorization": f"Basic {self.api_key}"
+        }
+
+        data = None
+
+        try:
+            response = requests.delete(url, headers=headers)
+            logger.info(f"{self.__class__.__name__} ( {self.delete_note.__name__} ) -- FUB API STATUS CODE - {response.status_code}")
+
+            data = response.json()
+            logger.debug(f"{self.__class__.__name__} ( {self.delete_note.__name__} ) -- FUB API DATA - {data}")
+
+        except Exception as ex:
+            logger.exception(f"{self.__class__.__name__} ( {self.delete_note.__name__} ) -- !!! FUB API ERROR - {ex}")
+
+        return data
