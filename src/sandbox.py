b7b5284ab3a1cbd68dcf5693a9640fb85b0944f9
ACTSE-FB4S <nikita@actse.ltd>
2024-05-20 14:46:39 +0000

    fix requirements.txt

--- src/sandbox.py
+++ src/sandbox.py
@@ -5,15 +5,7 @@ from config.database import (
     )
 
 
-postgres.connect(
-    ssh_mode=SSH_TUNNEL_MODE,
-    ssh_key_path=SSH_KEY_PATH,
-    ssh_server_host=SSH_TUNNEL_SERVER_HOST,
-    ssh_server_port=SSH_TUNNEL_SERVER_PORT,
-    ssh_username=SSH_TUNNEL_SERVER_USERNAME,
-    local_bind_address=("localhost", LOCAL_PORT),
-    remote_bind_address=(POSTGRES_HOST, POSTGRES_PORT)
-)
+postgres.connect()
 
 
 postgres.select_executor(
