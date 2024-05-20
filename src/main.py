08a19753c90e4f55a53d302deb74be31ab1d1729
ACTSE-FB4S <nikita@actse.ltd>
2024-05-20 14:06:49 +0000

    Configure docker

--- src/main.py
+++ src/main.py
@@ -15,6 +15,6 @@ async def index_view():
 
 if __name__ == "__main__":
     # module : app
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
     uvicorn.run("main:app", host="0.0.0.0", port=5003, reload=True)
