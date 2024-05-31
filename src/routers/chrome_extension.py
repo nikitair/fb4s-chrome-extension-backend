2717a2b20808f48ae1e90afea0d1dabf1cbce96e
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-31 17:39:50 +0300

    fix demo buyer response

--- src/routers/chrome_extension.py
+++ src/routers/chrome_extension.py
@@ -9,14 +9,14 @@ from typing import Union
 ce_router = APIRouter()
 
 
-@ce_router.get("/", response_model = DefaultResponse)
+@ce_router.get("/", response_model=DefaultResponse)
 async def chrome_extension_index():
     return {
         "success": True,
         "service": "FB4S Automations",
         "router": "Chrome Extension",
     }
-    
+
 
 @ce_router.get(
     "/profiles/buyer",
@@ -26,21 +26,23 @@ async def chrome_extension_index():
     },
 )
 async def get_buyer_profile(
-    access_level_key: str = Query(None, description="BASE64 of team_member@fb4s.com"), 
-    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"), 
+    access_level_key: str = Query(
+        None, description="BASE64 of team_member@fb4s.com"),
+    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"),
     profile_ikey: str = Query(None, description="BASE64 of buyer_customer_id")
 ):
     logger.debug(f"access_level_key: {access_level_key}")
     logger.debug(f"profile_ekey: {profile_ekey}")
     logger.debug(f"profile_ikey: {profile_ikey}")
 
-    result = services.get_buyer_profile(access_level_key, profile_ekey, profile_ikey)
+    result = services.get_buyer_profile(
+        access_level_key, profile_ekey, profile_ikey)
     if not result:
         logger.error("Buyer not found")
         return JSONResponse(
             content={"error": "Buyer NOT found"}, status_code=404
         )
-    
+
     logger.info(f"RESPONSE - {result}")
     return result
 
@@ -53,8 +55,9 @@ async def get_buyer_profile(
     },
 )
 async def get_demo_buyer_profile(
-    access_level_key: str = Query(None, description="BASE64 of team_member@fb4s.com"), 
-    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"), 
+    access_level_key: str = Query(
+        None, description="BASE64 of team_member@fb4s.com"),
+    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"),
     profile_ikey: str = Query(None, description="BASE64 of buyer_customer_id")
 ):
     return {
@@ -67,15 +70,14 @@ async def get_demo_buyer_profile(
         "province": "British Columbia",
         "fub_stage": "A - Hot",
         "registration_time": "07:00 PM - 20 May 2024",
-        "buyer_time_zone": "America/Vancouver",
-        "buyer_local_time": "02:30 AM - 31 May 2024",
+        "buyer_time_zone": -10,
         "lead_score": 3.5,
         "assigned_realtor_name": "Dave Realtor",
         "assigned_realtor_email": "dave.realtor@fb4s.com",
-        "profile_completed_levels": [
-            "intro",
-            "complete",
-            "supplemental"
-        ],
+        "profile_completed_levels": {
+            "intro": True,
+            "complete": True,
+            "supplemental": False
+        },
         "show_contacts": True
     }
