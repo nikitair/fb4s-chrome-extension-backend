f19aceec29b776be806e5020e528ef4265387223
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-31 17:16:43 +0300

    Update Buyer Profile model

--- src/schemas/chrome_extension.py
+++ src/schemas/chrome_extension.py
@@ -1,28 +1,11 @@
 from pydantic import BaseModel, Field
 
-{
-    "id": 12345,
-    "email": "john.johnson@mail.com",
-    "phone_number": "123-123-1234",
-    "first_name": "John",
-    "last_name": "Johnson",
-    "city": "Vancouver",
-    "province": "British Columbia",
-    "fub_stage": "A - Hot",
-    "registration_time": "07:00 PM - 20 May 2024",
-    "buyer_time_zone": "America/Vancouver",
-    "buyer_local_time": "02:30 AM - 31 May 2024",
-    "lead_score": 3.5,
-    "assigned_realtor_name": "Dave Realtor",
-    "assigned_realtor_email": "dave.realtor@fb4s.com",
-    "profile_completed_levels": [
-        "intro",
-        "complete",
-        "supplemental"
-    ],
-    "show_contacts": True
-}
 
+class ProfileCompletedLevels(BaseModel):
+    intro: bool = False
+    complete: bool = False
+    supplemental: bool = False
+    
 
 class GetBuyerProfileResponse(BaseModel):
     id: int
@@ -34,18 +17,12 @@ class GetBuyerProfileResponse(BaseModel):
     province: str | None = None
     fub_stage: str = "Not a FUB Buyer"
     registration_time: str | None = None
-    buyer_time_zone: str | None = None
-    buyer_local_time: str | None = None
-    lead_score: float | None = None
+    buyer_time_zone: int = 0
+    lead_score: float = 0.0
     assigned_realtor_name: str | None = None
     assigned_realtor_email: str | None = None
-    profile_completed_levels: list[str] = [
-        "intro",
-        "complete",
-        "supplemental"
-    ]
-    show_contacts: bool = Field(
-        True, description="Hide contact information (email, phone_number) if false")
+    profile_completed_levels: ProfileCompletedLevels
+    show_contacts: bool = True
 
 
 class BuyerNotFoundResponse(BaseModel):
