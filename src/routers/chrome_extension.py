f01c935c99a89383e53e50c4ece865e5c044c135
Nikita Stoliarov <stlrvnk@gmail.com>
2024-05-31 16:40:06 +0300

    Add demo buyer-profile endpoint

--- src/routers/chrome_extension.py
+++ src/routers/chrome_extension.py
@@ -43,3 +43,39 @@ async def get_buyer_profile(
     
     logger.info(f"RESPONSE - {result}")
     return result
+
+
+@ce_router.get(
+    "/demo-buyer",
+    responses={
+        200: {"model": schemas.GetBuyerProfileResponse, "description": "Buyer Profile"},
+        404: {"model": schemas.BuyerNotFoundResponse, "description": "Buyer NOT Found"}
+    },
+)
+async def get_demo_buyer_profile(
+    access_level_key: str = Query(None, description="BASE64 of team_member@fb4s.com"), 
+    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"), 
+    profile_ikey: str = Query(None, description="BASE64 of buyer_customer_id")
+):
+    return {
+        "id": 12345,
+        "email": "john.johnson@mail.com",
+        "phone_number": "123-123-1234",
+        "first_name": "John",
+        "last_name": "Johnson",
+        "city": "Vancouver",
+        "province": "British Columbia",
+        "fub_stage": "A - Hot",
+        "registration_time": "07:00 PM - 20 May 2024",
+        "buyer_time_zone": "America/Vancouver",
+        "buyer_local_time": "02:30 AM - 31 May 2024",
+        "lead_score": 3.5,
+        "assigned_realtor_name": "Dave Realtor",
+        "assigned_realtor_email": "dave.realtor@fb4s.com",
+        "profile_completed_levels": [
+            "intro",
+            "complete",
+            "supplemental"
+        ],
+        "show_contacts": True
+    }
--- src/schemas/chrome_extension.py
+++ src/schemas/chrome_extension.py
@@ -1,13 +1,52 @@
 from pydantic import BaseModel, Field
 
+{
+    "id": 12345,
+    "email": "john.johnson@mail.com",
+    "phone_number": "123-123-1234",
+    "first_name": "John",
+    "last_name": "Johnson",
+    "city": "Vancouver",
+    "province": "British Columbia",
+    "fub_stage": "A - Hot",
+    "registration_time": "07:00 PM - 20 May 2024",
+    "buyer_time_zone": "America/Vancouver",
+    "buyer_local_time": "02:30 AM - 31 May 2024",
+    "lead_score": 3.5,
+    "assigned_realtor_name": "Dave Realtor",
+    "assigned_realtor_email": "dave.realtor@fb4s.com",
+    "profile_completed_levels": [
+        "intro",
+        "complete",
+        "supplemental"
+    ],
+    "show_contacts": True
+}
+
 
 class GetBuyerProfileResponse(BaseModel):
-    buyer_customer_id: int
-    buyer_email: str
-    buyer_phone_number: str
-    buyer_first_name: str
-    buyer_last_name: str
-    show_contacts: bool = Field(True, description="Hide contact information (email, phone_number) if false")
+    id: int
+    email: str
+    phone_number: str
+    first_name: str
+    last_name: str
+    city: str | None = None
+    province: str | None = None
+    fub_stage: str = "Not a FUB Buyer"
+    registration_time: str | None = None
+    buyer_time_zone: str | None = None
+    buyer_local_time: str | None = None
+    lead_score: float | None = None
+    assigned_realtor_name: str | None = None
+    assigned_realtor_email: str | None = None
+    profile_completed_levels: list[str] = [
+        "intro",
+        "complete",
+        "supplemental"
+    ]
+    show_contacts: bool = Field(
+        True, description="Hide contact information (email, phone_number) if false")
+
 
 class BuyerNotFoundResponse(BaseModel):
     error: str = "Buyer NOT Found"
