9bfdc6a0bc1a97d60bf047d808d9362719d98128
ACTSE-FB4S <nikita@actse.ltd>
2024-06-04 09:28:49 +0000

    Edit query params docs

--- src/routers/chrome_extension.py
+++ src/routers/chrome_extension.py
@@ -29,8 +29,8 @@ async def chrome_extension_index():
 async def get_buyer_profile(
     access_level_key: str = Query(
         None, description="BASE64 of team_member@fb4s.com"),
-    profile_ekey: str = Query(None, description="BASE64 of buyer@mail.com"),
-    profile_ikey: str = Query(None, description="BASE64 of buyer_chat_id")
+    profile_ekey: str = Query(None, description="PipeDrive: BASE64 of buyer@mail.com"),
+    profile_ikey: str = Query(None, description="FUB: BASE64 of buyer_chat_id")
 ):
     logger.info("*** GET BUYER PROFILE TRIGGERED")
     return services.get_buyer_profile(access_level_key, profile_ekey, profile_ikey)
