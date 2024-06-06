98ce8abc7a3ffac9481a443fb3506fb07d86e8f1
Nikita Stoliarov <stlrvnk@gmail.com>
2024-06-06 11:45:11 +0300

    Temp solution

--- src/services/tools.py
+++ src/services/tools.py
@@ -6,37 +6,43 @@ from schemas.tools import Base64Response
 def encode_to_base64(item) -> str:
     logger.info(f"ENCODING TO BASE64 - {item}")
     
-    response = Base64Response()
-    response.mode = "encode"
+    response_dict = {
+        "success": True,
+        "mode": "encode",
+        "data": None
+    }
     
     try:
         encoded_item = base64.b64encode(item.encode()).decode()
         logger.info(f"BASE64 ENCODING RESULT - {encoded_item}")
     except (TypeError, ValueError) as ex:
-        response.success = False
+        response_dict["success"] = False
         logger.exception(f"!!! FAILED ENCODING TO BASE64 - {ex}")
     
     # TODO: find better solution
-    response_dict = dict(response.model_dump())
+    # response_dict = dict(response.model_dump())
     logger.info(f"RESPONSE - {response_dict}")
     return response_dict
-
 
 
 def decode_from_base64(item) -> str:
     logger.info(f"DECODING FROM BASE64 - {item}")
     
-    response = Base64Response()
-    response.mode = "decode"
+    # response = Base64Response()
+    response_dict = {
+        "success": True,
+        "mode": "decode",
+        "data": None
+    }
     
     try:
         decoded_item = base64.b64decode(item).decode()
         logger.info(f"BASE64 DECODING RESULT - {decoded_item}")
     except (TypeError, ValueError) as ex:
-        response.success = False
+        response_dict["success"] = False
         logger.exception(f"!!! FAILED DECODING FROM BASE64 - {ex}")
     
     # TODO: find better solution
-    response_dict = dict(response.model_dump())
+    # response_dict = dict(response.model_dump())
     logger.info(f"RESPONSE - {response_dict}")
     return response_dict
