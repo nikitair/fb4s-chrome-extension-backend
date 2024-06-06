f01631d4b19b30f537074aeef229e85112d9a394
Nikita Stoliarov <stlrvnk@gmail.com>
2024-06-06 11:36:07 +0300

    Fix 4))

--- src/services/tools.py
+++ src/services/tools.py
@@ -17,7 +17,7 @@ def encode_to_base64(item) -> str:
         logger.exception(f"!!! FAILED ENCODING TO BASE64 - {ex}")
     
     # TODO: find better solution
-    return response.dict()
+    return response
 
 
 
@@ -35,4 +35,4 @@ def decode_from_base64(item) -> str:
         logger.exception(f"!!! FAILED DECODING FROM BASE64 - {ex}")
     
     # TODO: find better solution
-    return response.dict()
+    return response
