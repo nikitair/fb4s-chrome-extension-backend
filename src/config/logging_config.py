9f63acd1979ac95ac750fecf96a814ddd67487ca
Nikita Stoliarov <stlrvnk@gmail.com>
2024-06-20 18:26:02 +0300

    add dev logs

--- src/config/logging_config.py
+++ src/config/logging_config.py
@@ -11,7 +11,8 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
     '%(asctime)s UTC - %(levelname)s - %(module)s:%(funcName)s:%(lineno)d - %(message)s')
 
-logs_file_path = os.path.join(ROOT_DIR, "logs", "logs.log")
+# logs_file_path = os.path.join(ROOT_DIR, "logs", "logs.log")
+logs_file_path = os.path.join(ROOT_DIR, "src", "dev_logs", "logs.log")
 
 # handlers
 terminal_handler = logging.StreamHandler()
